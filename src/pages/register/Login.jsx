9213a73c656d87501f69d97a6231bb35d3b2ec83
Abduraxim Berdiqobilov <abduraximberdiqobilov@Abduraxims-MacBook-Air.local>
2024-12-11 00:53:52 +0500

    error fixed

--- src/pages/register/Login.jsx
+++ src/pages/register/Login.jsx
@@ -28,7 +28,6 @@ const Login = () => {
     }
   };
 
-  console.log(users);
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
